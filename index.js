e79a8bc68550ba5f2a7a86eadf9ed06ea1fd7cdd
Amanda Smith <ajs.amandasmith@gmail.com>
2022-02-28 18:01:30 -0600

    commit tree 44286d0701558bb458e8bae018bc50be69f331ba

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+  name: "Amanda",
+  streetAddress: "mine"
+};
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+  const newObj = {...employeeObj};
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(empObj, key, value) {
+  empObj[key] = value;
+  return empObj;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newObj = {...employee};
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+console.log(employee);
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', "Sam"));
